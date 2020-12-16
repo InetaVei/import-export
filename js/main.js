ef68433c14f9740c8c6aa22b90967d1d7f0b3a58
InetaVei <74532995+InetaVei@users.noreply.github.com>
2020-12-16 23:18:28 +0200

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+import { suma } from './components/suma.js';
+import { atimtis } from './components/atimtis.js';
+import { daugyba } from './components/daugyba.js';
+import { dalyba } from './components/dalyba.js';
+import { vidurkis } from './components/vidurkis.js';
+
+
+const ats1 = suma(10, 5);
+console.log(ats1);
+
+const ats2 = atimtis(10, 5);
+console.log(ats2);
+
+const ats3 = daugyba(10, 5);
+console.log(ats3);
+
+const ats4 = dalyba(10, 5);
+console.log(ats4);
+
+const ats5 = vidurkis([10, 2, 8, 4]);
+console.log(ats5);
